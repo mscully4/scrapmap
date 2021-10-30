e9b5a700370f44f508313aa3bbf1d4db9d3b20f1
Michael <michael.scully1997@gmail.com>
2021-10-30 02:12:29 -0400

    /auth/verify_user implementation

---
+++ auth/verify_user/lambda_function.py
@@ -0,0 +1,79 @@
+import json
+import boto3
+import botocore.exceptions
+import hmac
+import hashlib
+import base64
+import uuid
+import os
+
+def get_secret_hash(username, client_id, client_secret):
+    msg = username + client_id
+    dig = hmac.new(str(client_secret).encode('utf-8'), 
+        msg = str(msg).encode('utf-8'), digestmod=hl.sha256).digest()
+    d2 = base64.b64encode(dig).decode()
+    return d2
+
+def lambda_handler(event, context):
+    #Validate Environment Variables
+    for env_var in ("CLIENT_ID", "CLIENT_SECRET"):
+        if env_var not in os.environ:
+            raise KeyError(f"Environemnt varibale {env_var} does not exist")
+
+    #Retrieve secrets stored as environment variables
+    client_id = os.environ['CLIENT_ID']
+    client_secret = os.environ["CLIENT_SECRET"]
+
+    body = json.loads(event['body'])
+
+    #Validate request data
+    missing_fields = []
+    for field in ["username", "code"]:
+        if field not in body:
+            missing_fields.append(field)
+ 
+    if missing_fields:
+        return {
+            "statusCode": 400,
+            "body": f"Missing fields: {missing_fields}"
+        }
+
+    username = body['username']
+    code = body['code']
+
+    try:
+        client = boto3.client('cognito-idp')
+
+        response = client.confirm_sign_up(
+            ClientId=client_id,
+            SecretHash=get_secret_hash(username, client_id, client_secret),
+            Username=username,
+            ConfirmationCode=code,
+            ForceAliasCreation=False,
+       )
+
+    except client.exceptions.UserNotFoundException:
+        return {
+            "statusCode": 404, 
+            "body": "Username doesnt exists"
+        }
+    except client.exceptions.CodeMismatchException:
+        return {
+            "statusCode": 400, 
+            "body": "Invalid verification code"
+        }
+    except client.exceptions.NotAuthorizedException:
+        return {
+            "statusCode": 400, 
+            "body": "User is already confirmed"
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": "Error. Please try again later", 
+        }
+    
+    #If there are no errors, the verification was successful
+    return {
+        "statusCode": 204
+    }
