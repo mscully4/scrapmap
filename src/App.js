ccffdd769226c524ef72072d213264a72e554250
Michael <michael.scully1997@gmail.com>
2021-12-08 02:57:31 -0500

    Add Auth Flow to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { memo, useEffect, useState } from 'react';
+import { refreshTokens } from './utils/backend'
 
-function App() {
+const componentDidMount = (setters) => {
+  if (localStorage.lastAuthUser) {
+    refreshTokens(localStorage.lastAuthUser, localStorage.refreshToken).then(resp => {
+      resp.json().then(json => {
+        localStorage.setItem("accessToken", json.access_token)
+        localStorage.setItem("idToken", json.id_token)
+        localStorage.setItem("tokenType", json.token_type)
+        setters.setIsLoggedIn(true)
+        setters.setUser(localStorage.lastAuthUser)
+      })
+    })
+
+  }
+}
+
+function App(props) {
+  const [user, setUser] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const setters = {
+    setUser: setUser,
+    setIsLoggedIn: setIsLoggedIn
+  }
+
+  useEffect(async () => {
+    componentDidMount(setters)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export default memo(App)
